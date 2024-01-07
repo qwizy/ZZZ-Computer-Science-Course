145354cb2fb9844fa70d0429ae554e7c69821311
Nikita Krushinin <144558570+qwizy@users.noreply.github.com>
2024-01-07 19:18:42 +0300

    Create shared_header.h

---
+++ tasks/finite_state_machine/tasks/1/shared_header.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum {
+    START,
+    COUNT, 
+    STATECOUNT,
+} StateName;
+
+typedef struct State State;
+
+typedef struct {
+    State* pipeline;
+    StateName cur_state;
+    size_t slash_count;
+    size_t word_count;
+    char current_char;
+    bool check_word;
+} StateMachine;
+
+typedef struct State {
+    StateName name;
+    StateName (*action)(StateMachine*);
+} State;
